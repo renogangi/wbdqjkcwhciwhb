ee95db7fc181696fb64b39da9ed35b16bc680184
reno <ragan3@student.monash.edu>
2017-08-24 13:04:49 +1000

    Set up assignment

---
+++ Application/app.js
@@ -0,0 +1,24 @@
+var admin = require("firebase-admin");
+var express = require("express");
+var app = express();
+var router = express.Router();
+var path = __dirname + '/views/';
+
+// Fetch the service account key JSON file contents
+var serviceAccount = require("./serviceAccountKey.json");
+
+router.use(function (req,res,next) {
+  console.log("/" + req.method);
+  next();
+});
+
+router.get("/",function(req,res){
+  res.sendFile(path + "index.html");
+});
+
+app.use("/",router);
+
+app.listen(3000,function(){
+  console.log("Live at Port 3000");
+});
+
